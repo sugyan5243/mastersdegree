03d8c59358f5c42c0e8e75c3785de6b079e11571
sugyan5243 <sugyan5243@shinichirousugiyama-no-MacBook-Pro.local>
2017-07-31 10:23:42 +0900

    20160425 add "UP" & "DOWN"

--- program/HakodasReImplementation/arduino/Recode.ino
+++ program/HakodasReImplementation/arduino/Recode.ino
@@ -1,69 +1,105 @@
 #define PIN 0
-#define TS 950
+#define PUSH 1
+#define COVER 2
 
-#define COVERMAX 900
-#define COVERPUSH 850
-#define PUSHMIN 700
+#define SENSOR 950
+#define COVERORPUSH 880
 
-#define DOWNMAX 950
-#define DOWNUP 935
-#define UPMIN 920
+#define CPUD 30
 
-int sensor;
-unsigned long sensum = 0;
-int senave;
-int count;
-int check;
+int count = 0;
+int check = 0;
+unsigned long sensum[CPUD] = {0,};
+int avebef;
+int aveaft;
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
+  int j;
+  int sensor;
+
+  //photorefrector check
   sensor = analogRead(PIN);
 
-  Identify_Gesture();
-  if((check == 0) && (count == 0)&&(sensor < TS)){
-    for(; sensor < TS;){
-      sensor = analogRead(PIN);
-      count++;
-      sensum += sensor;
+  //COVER
+  if(check == COVER){
+    Serial.println("COVER");
+    check = 0;
+    for(j = 0; j < CPUD; j++){
+      sensum[j] = 0;
+    }
+    count = 0;
+    delay(500);
 
-      if(count >= 30){
-        check = Ccheck();
+  //PUSH
+  }else if(check == PUSH){
+    Serial.println("PUSH");
+    check = 0;
+    for(j = 0; j < CPUD; j++){
+      sensum[j] = 0;
+    }
+    count = 0;
+    delay(500);
+
+  //UP or DOWN
+  }else if((check == 0)&&(count != 0)){
+    avebef = 0;
+    aveaft = 0;
+
+    Serial.println(count);
+    for(j = 0; j < count/2; j++){
+      avebef += sensum[j];
+      sensum[j] = 0;
+    }
+    for(j = count/2; j < count; j++){
+      aveaft += sensum[j];
+      sensum[j] = 0;
+    }
+
+    Serial.print(avebef);
+    Serial.print(" ");
+    Serial.println(aveaft);
+
+    if(avebef > aveaft){
+      Serial.println("UP");
+    }else{
+      Serial.println("DOWN");
+    }
+
+    count = 0;
+    delay(500);
+  }
+
+  //other,finger is now
+  else if(sensor < SENSOR){
+    for(count = 0; sensor < SENSOR; count++){
+      sensor = analogRead(PIN);
+
+      //COVER or PUSH
+      if(count >= CPUD){
+        aveaft = 0;
+        for(j = 0;j < CPUD;j++){
+          aveaft += sensum[j];
+        }
+        aveaft = aveaft/CPUD;
+        if((700 < aveaft)&&(aveaft <= COVERORPUSH)){
+          check = PUSH;
+        }else if((COVERORPUSH < aveaft)&&(aveaft <= 950)){
+          check = COVER;
+        }
+          sensor = 1000;
+      }else{
+         //check process
+         sensum[count] += sensor;
       }
+      Serial.println(count);
       delay(10);
     }
   }
   delay(10);
-}
-
-int Ccheck(){
-  senave = sensum/count;
-  if((PUSHMIN < senave) && (senave <= COVERPUSH)){
-    return 1;
-  }else if((COVERPUSH < senave) && (senave <= COVERMAX)){
-    return 2;
-  }
-}
-
-void Identify_Gesture(){
-  if(check == 1){
-    Serial.println("PUSH");
-  }else if(check == 2){
-    Serial.println("COVER");
-  }else if((check == 0) && (count != 0)){
-    senave = sensum/count;
-    if((UPMIN < senave) && (senave <= DOWNUP)){
-      Serial.println("UP");
-    }else if((DOWNUP < senave) && (senave <= DOWNMAX)){
-      Serial.println("DOWN");
-    }
-  }
-
-  check = 0;
-  count = 0;
-  sensum = 0;
-  delay(500);
 }
 
