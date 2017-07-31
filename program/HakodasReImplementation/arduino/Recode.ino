648ef72483fc4e28d1ffadd162cc839ce1a7551c
sugyan5243 <sugyan5243@shinichirousugiyama-no-MacBook-Pro.local>
2017-07-31 10:34:33 +0900

    20160430 modify Recode to Function's Program

--- program/HakodasReImplementation/arduino/Recode.ino
+++ program/HakodasReImplementation/arduino/Recode.ino
@@ -2,15 +2,14 @@
 #define PUSH 1
 #define COVER 2
 
-#define SENSOR 950
-#define COVERORPUSH 880
+#define SENSOR 1020
+#define CORP 180
 
 #define CPUD 30
 
 int count = 0;
 int check = 0;
-unsigned long sensum[CPUD] = {0,};
-int avebef;
+int sensum[CPUD] = {0,};
 int aveaft;
 
 void setup() {
@@ -27,50 +26,18 @@ void loop() {
 
   //COVER
   if(check == COVER){
-    Serial.println("COVER");
-    check = 0;
-    for(j = 0; j < CPUD; j++){
-      sensum[j] = 0;
-    }
-    count = 0;
+    Identify(sensum);
+    Covers(&check, &count);
     delay(500);
-
   //PUSH
   }else if(check == PUSH){
-    Serial.println("PUSH");
-    check = 0;
-    for(j = 0; j < CPUD; j++){
-      sensum[j] = 0;
-    }
-    count = 0;
+    Identify(sensum);
+    Pushs(&check, &count);
     delay(500);
 
   //UP or DOWN
   }else if((check == 0)&&(count != 0)){
-    avebef = 0;
-    aveaft = 0;
-
-    Serial.println(count);
-    for(j = 0; j < count/2; j++){
-      avebef += sensum[j];
-      sensum[j] = 0;
-    }
-    for(j = count/2; j < count; j++){
-      aveaft += sensum[j];
-      sensum[j] = 0;
-    }
-
-    Serial.print(avebef);
-    Serial.print(" ");
-    Serial.println(aveaft);
-
-    if(avebef > aveaft){
-      Serial.println("UP");
-    }else{
-      Serial.println("DOWN");
-    }
-
-    count = 0;
+    UpDown(&count, sensum);
     delay(500);
   }
 
@@ -86,20 +53,97 @@ void loop() {
           aveaft += sensum[j];
         }
         aveaft = aveaft/CPUD;
-        if((700 < aveaft)&&(aveaft <= COVERORPUSH)){
-          check = PUSH;
-        }else if((COVERORPUSH < aveaft)&&(aveaft <= 950)){
+        if(aveaft < CORP){
           check = COVER;
+        }else{
+          check = PUSH;
         }
-          sensor = 1000;
+          sensor = 10000;
       }else{
          //check process
-         sensum[count] += sensor;
+         sensum[count] += 1023-sensor;
       }
-      Serial.println(count);
+      Serial.println(sensum[count]);
       delay(10);
     }
   }
-  delay(10);
+  delay(100);
+}
+
+
+void Identify(int *sensum){
+  int i = 0;
+  for(i = 0;i < CPUD; i++){
+    sensum[i] = 0;
+  }
+}
+
+
+void Covers(int *chk, int *cnt){
+    Serial.println("COVER");
+    *chk = 0;
+    *cnt = 0;
+}
+
+
+void Pushs(int *chk, int *cnt){
+  Serial.println("PUSH");
+  *chk = 0;
+  *cnt = 0;
+}
+
+
+void UpDown(int *cnt, int *sensum){
+    int bef = 0;
+    int aft = 0;
+    int i;
+
+    Serial.println(*cnt);
+    for(i = 0; i < *cnt/2; i++){
+      bef += sensum[i];
+      sensum[i] = 0;
+    }
+    bef = bef/(*cnt/2);
+    for(i = *cnt/2; i < *cnt; i++){
+      aft += sensum[i];
+      sensum[i] = 0;
+    }
+    aft = aft/(*cnt/2);
+
+    Serial.print(bef);
+    Serial.print(" ");
+    Serial.println(aft);
+
+    if(bef > aft){
+      Serial.println("UP");
+    }else{
+      Serial.println("DOWN");
+    }
+
+    *cnt = 0;
+}
+
+
+void UpDown2(int *cnt, int *sensum){
+    int bef = 0;
+    int aft = 0;
+    int i;
+
+    Serial.println(*cnt);
+    for(i = *cnt-5; i < *cnt; i++){
+      aft += sensum[i];
+      sensum[i] = 0;
+    }
+    aft = aft/4;
+    Serial.print("It's ");
+    Serial.println(aft);
+
+    if(aft < 100){
+      Serial.println("UP");
+    }else{
+      Serial.println("DOWN");
+    }
+
+    *cnt = 0;
 }
 
