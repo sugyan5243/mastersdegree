d5c83dbe6d97de9d407b6e7974c1f2c04cba6732
sugyan5243 <sugyan5243@shinichirousugiyama-no-MacBook-Pro.local>
2017-07-31 10:21:07 +0900

    20160422 modify Recode to Function's Program

--- program/HakodasReImplementation/arduino/Recode.ino
+++ program/HakodasReImplementation/arduino/Recode.ino
@@ -1,79 +1,69 @@
-int pin = 0;
-int yline = 950;
-int cors = 0; //Time
+#define PIN 0
+#define TS 950
 
-int check = 0;
-unsigned long yubinum = 0;
-int yave;
-int PUSH = 1;
-int COVER = 2;
+#define COVERMAX 900
+#define COVERPUSH 850
+#define PUSHMIN 700
+
+#define DOWNMAX 950
+#define DOWNUP 935
+#define UPMIN 920
+
+int sensor;
+unsigned long sensum = 0;
+int senave;
+int count;
+int check;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  int yubi;
+  sensor = analogRead(PIN);
 
-  //photorefrector check
-  yubi = analogRead(pin);
+  Identify_Gesture();
+  if((check == 0) && (count == 0)&&(sensor < TS)){
+    for(; sensor < TS;){
+      sensor = analogRead(PIN);
+      count++;
+      sensum += sensor;
 
-  //COVER
-  if(check == COVER){
-    Serial.println("COVER");
-    check = 0;
-    cors = 0;
-    yubinum = 0;
-    delay(500);
-
-  //PUSH
-  }else if(check == PUSH){
-    Serial.println("PUSH");
-    check = 0;
-    cors = 0;
-    yubinum = 0;
-    delay(500);
-
-  //UP or DOWN
-  }else if((check == 0)&&(cors != 0)){
-    yave = yubinum/cors;
-    if((900 <= yave)&&(yave < 938)){
-      Serial.println("UP");
-    }if((938 <= yave)&&(yave < 950)){
-      Serial.println("DOWN");
-    }
-
-//    Serial.print(yubinum); Serial.print(" "); Serial.println(cors);
-    Serial.println(yubinum/cors);
-
-    cors = 0;
-    yubinum = 0;
-    delay(500);
-  }
-
-  //other,finger is now
-  else if(yubi < yline){
-    for(; yubi < yline;){
-      yubi = analogRead(pin);
-
-       //check process
-      cors++;
-      yubinum += yubi;
-
-      //COVER or PUSH
-      if(cors >= 30){
-        yave = yubinum/cors;
-        if((700 < yave)&&(yave <= 850)){
-          check = PUSH;
-        }else if((850 < yave)&&(yave <= 950)){
-          check = COVER;
-        }
+      if(count >= 30){
+        check = Ccheck();
       }
-
       delay(10);
     }
   }
   delay(10);
+}
+
+int Ccheck(){
+  senave = sensum/count;
+  if((PUSHMIN < senave) && (senave <= COVERPUSH)){
+    return 1;
+  }else if((COVERPUSH < senave) && (senave <= COVERMAX)){
+    return 2;
+  }
+}
+
+void Identify_Gesture(){
+  if(check == 1){
+    Serial.println("PUSH");
+  }else if(check == 2){
+    Serial.println("COVER");
+  }else if((check == 0) && (count != 0)){
+    senave = sensum/count;
+    if((UPMIN < senave) && (senave <= DOWNUP)){
+      Serial.println("UP");
+    }else if((DOWNUP < senave) && (senave <= DOWNMAX)){
+      Serial.println("DOWN");
+    }
+  }
+
+  check = 0;
+  count = 0;
+  sensum = 0;
+  delay(500);
 }
 
